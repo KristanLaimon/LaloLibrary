7c01947a633e64f817898d312668adf6ba3d220d
Kristan Ruíz <itan_07@hotmail.com>
2023-10-09 07:35:19 -0700

    Trying to improve Bitmap with a better approach (a rework?)

--- ControlsUtils/BitmapUtils.cs
+++ ControlsUtils/BitmapUtils.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace LaloLibrary.FormsUtils.Bitmaps
 {
     public class BitmapUtils
@@ -18,25 +19,41 @@ namespace LaloLibrary.FormsUtils.Bitmaps
         /// <param name="bitmap"></param>
         public static void UpdatePictureBox(PictureBox picturebox, Bitmap bitmap) => picturebox.Image = bitmap;
 
-        public static void MakeBitmapBlank(Bitmap bitmap)
+        public static Bitmap MakeBitmapBlank(Bitmap bitmap)
         {
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 g.Clear(Color.White);
             }
+
+            return bitmap;
         }
 
-        public static void PaintBitmap(Bitmap bitmap, Color color)
+        public static Bitmap PaintBitmap(Bitmap bitmap, Color color)
         {
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 g.Clear(color);
             }
+            return bitmap;
         }
 
         public static Bitmap GetDimensionsFromPictureBox(PictureBox picturebox)
         {
             return new Bitmap(picturebox.Width, picturebox.Height);
         }
+    }
+
+    //I'm thinking to remove static and make them part of the object itself: a new bitmap extended class??
+    partial class BetterBitmap
+    {
+        private Bitmap bitmap;
+
+        public BetterBitmap(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+        }
+
+        //Add functions and extended logic here
     }
 }
